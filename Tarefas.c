194e9eeae3abc03cbb26eac0981ce746b614800b
lhsmf <38105337+lhsmf@users.noreply.github.com>
2018-10-01 14:03:22 -0300

    Update and rename Tarefas update 1.c to Tarefas.c

--- Tarefas.c
+++ Tarefas.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node{//estrutura da tarefa, precisa de complementos
+struct Node {//estrutura da tarefa, não sei como fzr isso
 	int num;
 	struct Node *prox;
-}; 
+};
 typedef struct Node node;
 
 int tam;
-
+//adicionando inputs possiveis para as tarefas em questão de prioridade
 void inicia(node *TAREFAS);
 int menu(void);
 void opcao(node *TAREFAS, int op);
@@ -17,7 +17,7 @@ void insereFim(node *TAREFAS);
 void insereInicio(node *TAREFAS);
 void exibe(node *TAREFAS);
 void libera(node *TAREFAS);
-void insere (node *TAREFAS);
+void insere(node *TAREFAS);
 node *retiraInicio(node *TAREFAS);
 node *retiraFim(node *TAREFAS);
 node *retira(node *TAREFAS);
@@ -25,36 +25,35 @@ node *retira(node *TAREFAS);
 
 int main(void)
 {
-	node *TAREFAS = (node *) malloc(sizeof(node));
-	if(!TAREFAS){
+	node *TAREFAS = (node *)malloc(sizeof(node));
+	if (!TAREFAS) {
 		printf("Sem memoria disponivel!\n");
 		exit(1);
 	}
-	else{
-	inicia(TAREFAS);
-	int opt;
-	
-	do{
-		opt=menu();
-		opcao(TAREFAS,opt);
-	}
-	while(opt);
+	else {
+		inicia(TAREFAS);
+		int opt;
 
-	    free(TAREFAS);
-	    return 0;
+		do {
+			opt = menu();
+			opcao(TAREFAS, opt);
+		} while (opt);
+
+		free(TAREFAS);
+		return 0;
 	}
 }
 
-void inicia(node *TAREFAS)
+void inicia(node *TAREFAS)//nada dentro da lista de tarefas
 {
 	TAREFAS->prox = NULL;
-	tam=0;
+	tam = 0;
 }
 
 int menu(void)
 {
 	int opt;
-	
+
 	printf("Escolha a opcao\n");
 	printf("0. Sair\n");
 	printf("1. Zerar lista\n");
@@ -66,62 +65,63 @@ int menu(void)
 	printf("7. Retirar do fim\n");
 	printf("8. Escolher de onde tirar\n");
 	printf("Opcao: "); scanf("%d", &opt);
-	
+
 	return opt;
 }
 
 void opcao(node *TAREFAS, int op)
 {
 	node *tmp;
-	switch(op){
-		case 0:
-			libera(TAREFAS);
-			break;
-			
-		case 1:
-			libera(TAREFAS);
-			inicia(TAREFAS);
-			break;
-		
-		case 2:
-			exibe(TAREFAS);
-			break;
-		
-		case 3:
-			insereInicio(TAREFAS);
-			break;		
-			
-		case 4:
-			insereFim(TAREFAS);
-			break;
-			
-		case 5:
-			insere(TAREFAS);
-			break;
-		
-		case 6:
-			tmp= retiraInicio(TAREFAS);
-			printf("Retirado: %3d\n\n", tmp->num);
-			break;
-			
-		case 7:
-			tmp= retiraFim(TAREFAS);
-			printf("Retirado: %3d\n\n", tmp->num);
-			break;
-		
-		case 8:
-			tmp= retira(TAREFAS);
-			printf("Retirado: %3d\n\n", tmp->num);
-			break;
-		
-		default:
-			printf("Comando invalido\n\n");
+	switch (op) {
+	case 0:
+		libera(TAREFAS);
+		break;
+
+	case 1:
+		libera(TAREFAS);
+		inicia(TAREFAS);
+		break;
+
+	case 2:
+		exibe(TAREFAS);
+		break;
+
+	case 3:
+		insereInicio(TAREFAS);
+		break;
+
+	case 4:
+		insereFim(TAREFAS);
+		break;
+
+	case 5:
+		insere(TAREFAS);
+		break;
+
+	case 6:
+		tmp = retiraInicio(TAREFAS);
+		printf("Retirado: %3d\n\n", tmp->num);
+		break;
+
+	case 7:
+		tmp = retiraFim(TAREFAS);
+		printf("Retirado: %3d\n\n", tmp->num);
+		break;
+
+	case 8:
+		tmp = retira(TAREFAS);
+		printf("Retirado: %3d\n\n", tmp->num);
+		break;
+
+	default:
+		printf("Comando invalido\n\n");
 	}
 }
 
+//fazendo as funcoes funcionarem
 int vazia(node *TAREFAS)
 {
-	if(TAREFAS->prox == NULL)
+	if (TAREFAS->prox == NULL)
 		return 1;
 	else
 		return 0;
@@ -129,11 +129,12 @@ int vazia(node *TAREFAS)
 
 node *aloca()
 {
-	node *novo=(node *) malloc(sizeof(node));
-	if(!novo){
+	node *novo = (node *)malloc(sizeof(node));
+	if (!novo) {
 		printf("Sem memoria disponivel!\n");
 		exit(1);
-	}else{
+	}
+	else {
 		printf("Novo elemento: "); scanf("%d", &novo->num);
 		return novo;
 	}
@@ -142,17 +143,17 @@ node *aloca()
 
 void insereFim(node *TAREFAS)
 {
-	node *novo=aloca();
+	node *novo = aloca();
 	novo->prox = NULL;
-	
-	if(vazia(TAREFAS))
-		TAREFAS->prox=novo;
-	else{
+
+	if (vazia(TAREFAS))
+		TAREFAS->prox = novo;
+	else {
 		node *tmp = TAREFAS->prox;
-		
-		while(tmp->prox != NULL)
+
+		while (tmp->prox != NULL)
 			tmp = tmp->prox;
-		
+
 		tmp->prox = novo;
 	}
 	tam++;
@@ -160,50 +161,50 @@ void insereFim(node *TAREFAS)
 
 void insereInicio(node *TAREFAS)
 {
-	node *novo=aloca();	
+	node *novo = aloca();
 	node *oldHead = TAREFAS->prox;
-	
+
 	TAREFAS->prox = novo;
 	novo->prox = oldHead;
-	
+
 	tam++;
 }
 
 void exibe(node *TAREFAS)
 {
 	system("clear");
-	if(vazia(TAREFAS)){
+	if (vazia(TAREFAS)) {
 		printf("sem tarefas!\n\n");
-		return ;
+		return;
 	}
-	
+
 	node *tmp;
 	tmp = TAREFAS->prox;
 	printf("Tarefas:");
-	while( tmp != NULL){
+	while (tmp != NULL) {
 		printf("%5d", tmp->num);
 		tmp = tmp->prox;
 	}
 	printf("\n        ");
 	int count;
-	for(count=0 ; count < tam ; count++)
+	for (count = 0; count < tam; count++)
 		printf("  ^  ");
 	printf("\nOrdem:");
-	for(count=0 ; count < tam ; count++)
-		printf("%5d", count+1);
-	
-		
+	for (count = 0; count < tam; count++)
+		printf("%5d", count + 1);
+
+
 	printf("\n\n");
 }
 
 void libera(node *TAREFAS)
 {
-	if(!vazia(TAREFAS)){
+	if (!vazia(TAREFAS)) {
 		node *proxNode,
-			  *atual;
-		
+			*atual;
+
 		atual = TAREFAS->prox;
-		while(atual != NULL){
+		while (atual != NULL) {
 			proxNode = atual->prox;
 			free(atual);
 			atual = proxNode;
@@ -217,59 +218,62 @@ void insere(node *TAREFAS)
 		count;
 	printf("Em que posicao, [de 1 ate %d] voce deseja inserir: ", tam);
 	scanf("%d", &pos);
-	
-	if(pos>0 && pos <= tam){
-		if(pos==1)
+
+	if (pos>0 && pos <= tam) {
+		if (pos == 1)
 			insereInicio(TAREFAS);
-		else{
+		else {
 			node *atual = TAREFAS->prox,
-				 *anterior=TAREFAS; 
-			node *novo=aloca();
-				 
-			for(count=1 ; count < pos ; count++){
-					anterior=atual;
-					atual=atual->prox;
+				*anterior = TAREFAS;
+			node *novo = aloca();
+
+			for (count = 1; count < pos; count++) {
+				anterior = atual;
+				atual = atual->prox;
 			}
-			anterior->prox=novo;
+			anterior->prox = novo;
 			novo->prox = atual;
 			tam++;
 		}
-			
-	}else
-		printf("Elemento invalido\n\n");		
+
+	}
+	else
+		printf("Elemento invalido\n\n");
 }
 
 node *retiraInicio(node *TAREFAS)
 {
-	if(TAREFAS->prox == NULL){
+	if (TAREFAS->prox == NULL) {
 		printf("Não há tarefas\n");
 		return NULL;
-	}else{
+	}
+	else {
 		node *tmp = TAREFAS->prox;
 		TAREFAS->prox = tmp->prox;
 		tam--;
 		return tmp;
 	}
-				
+
 }
 
 node *retiraFim(node *TAREFAS)
 {
-	if(TAREFAS->prox == NULL){
+	if (TAREFAS->prox == NULL) {
 		printf("Não há tarefas para excluir\n\n");
 		return NULL;
-	}else{
+	}
+	else {
 		node *ultimo = TAREFAS->prox,
-			 *penultimo = TAREFAS;
-			 
-		while(ultimo->prox != NULL){
+			*penultimo = TAREFAS;
+
+		while (ultimo->prox != NULL) {
 			penultimo = ultimo;
 			ultimo = ultimo->prox;
 		}
-			 
+
 		penultimo->prox = NULL;
 		tam--;
-		return ultimo;		
+		return ultimo;
 	}
 }
 
@@ -279,25 +283,26 @@ node *retira(node *TAREFAS)
 		count;
 	printf("Que posicao, [de 1 ate %d] voce deseja retirar: ", tam);
 	scanf("%d", &opt);
-	
-	if(opt>0 && opt <= tam){
-		if(opt==1)
+
+	if (opt>0 && opt <= tam) {
+		if (opt == 1)
 			return retiraInicio(TAREFAS);
-		else{
+		else {
 			node *atual = TAREFAS->prox,
-				 *anterior=TAREFAS; 
-				 
-			for(count=1 ; count < opt ; count++){
-				anterior=atual;
-				atual=atual->prox;
+				*anterior = TAREFAS;
+
+			for (count = 1; count < opt; count++) {
+				anterior = atual;
+				atual = atual->prox;
 			}
-			
-		anterior->prox=atual->prox;
-		tam--;
-		return atual;
+
+			anterior->prox = atual->prox;
+			tam--;
+			return atual;
 		}
-			
-	}else{
+
+	}
+	else {
 		printf("Elemento invalido\n\n");
 		return NULL;
 	}
